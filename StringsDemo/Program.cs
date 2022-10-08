9a77ebf8ed148f4faefa1c63bedc194c74c5c7a3
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-10-07 18:05:54 -0600

    Add project files.

---
+++ StringsDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
